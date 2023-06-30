6fa8cb504fee37f15b60dd1d79eae00f7abbb133
Shivam kumar <114915415+Shivamkumar1352@users.noreply.github.com>
2023-07-01 03:21:40 +0530

    Add files via upload

---
+++ 12_zSubarray2.cpp
@@ -0,0 +1,35 @@
+// Longest Arithmatic Subarray
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    
+    int ans=2;
+    int d = a[1] - a[0];
+    int j = 2;
+    int curr =2;
+    while(j<n)
+    {
+        if(a[j]-a[j-1]==d)
+        curr++;
+        else
+        {
+            d = a[j]-a[j-1];
+            curr=2;
+        }
+        if(curr>ans)
+        ans=curr;
+        j++;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
